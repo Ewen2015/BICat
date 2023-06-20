78a353898fdb9085e757ad313482f235564c0be7
Ewen Wang <wang.enqun@outlook.com>
2023-06-20 17:08:52 +0800

    update

--- app/app.py
+++ app/app.py
@@ -59,10 +59,10 @@ if uploaded_file :
         st.session_state['history'] = []
 
     if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
+        st.session_state['generated'] = ["Hello! Ask me anything about " + uploaded_file.name + " 🤗"]
 
     if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
+        st.session_state['past'] = ["Hey! 👋"]
         
     #container for the chat history
     response_container = st.container()
@@ -72,7 +72,7 @@ if uploaded_file :
     with container:
         with st.form(key='my_form', clear_on_submit=True):
             
-            user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
+            user_input = st.text_input("Query:", placeholder="Talk about your csv data here : )", key='input')
             submit_button = st.form_submit_button(label='Send')
             
         if submit_button and user_input:
@@ -85,4 +85,4 @@ if uploaded_file :
         with response_container:
             for i in range(len(st.session_state['generated'])):
                 message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="big-smile")
